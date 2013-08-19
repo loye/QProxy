6ea4d4747eedc596dc156a855994eb5e2064eb27
Loye <loye.qiu@gmail.com>
2013-08-19 10:59:57 +0800

    TC

---
+++ HttpTunnelWeb/DebugHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Web;
+
+namespace Q.Net.Web
+{
+    public class DebugHandler : IHttpHandler
+    {
+        #region IHttpHandler Members
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var request = context.Request;
+            var response = context.Response;
+
+            //ThreadPool.SetMinThreads(1000, 1000);
+
+            int minWorkerThreads, minCompletionPortThreads, maxWorkerThreads, maxCompletionPortThreads;
+            ThreadPool.GetMinThreads(out minWorkerThreads, out minCompletionPortThreads);
+            ThreadPool.GetMaxThreads(out maxWorkerThreads, out maxCompletionPortThreads);
+
+            response.Write("<table>");
+
+            response.Write(GetTableLine("MinWorkerThreads", minWorkerThreads.ToString()));
+            response.Write(GetTableLine("MinCompletionPortThreads", minCompletionPortThreads.ToString()));
+            response.Write(GetTableLine("MaxWorkerThreads", maxWorkerThreads.ToString()));
+            response.Write(GetTableLine("MaxCompletionPortThreads", maxCompletionPortThreads.ToString()));
+
+            response.Write("</table>");
+        }
+
+        private string GetTableLine(string name, string value)
+        {
+            return String.Format("<tr><td>{0}</td><td>{1}</td></tr>", name, value);
+        }
+
+        #endregion
+    }
+}
